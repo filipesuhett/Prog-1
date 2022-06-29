9ffb5efbddead0cffedec9afff293c77ce6b416d
Filipe Suhett <filipe.gmx@gmail.com>
2022-06-28 23:35:21 -0300

    .

--- 07. LVP Prova 2/principal.py
+++ 07. LVP Prova 2/principal.py
@@ -14,83 +14,61 @@
 
 Será necessário incluir uma validação de dados que só aceite valores de 1 a 11'''
 
-from secundaria import f_tam
-from secundaria import f_mdc
-from secundaria import f_primo
-from secundaria import f_verificaOpcao
-from secundaria import f_verificaArmstrong
-from secundaria import f_inverte
-from secundaria import f_capicua
-from secundaria import f_perfeito
-from secundaria import f_decToBin
-from secundaria import f_decToHex
-from secundaria import f_contPrimo
+import sf
 
 def main():
 #n = numero
 #m = operação
 #s = soma
 #c = contador
-#0 = verificador
-#mi = maior numero
-#mn = menor numero
   
   s = 0
   c = 0
   o = True
   m = int(input())
 
-  while (not f_verificaOpcao(m)):
+  while (not sf.f_verificaOpcao(m)):
     m = int(input('OPCAO INVALIDA. TENTE OUTRA VEZ:'))
   
   while (m != 11):
     n = int(input())
     s += n
     c += 1
-
-    if (o):
-      mi = n
-      mn = n
-      o = False
-    elif (n > mi):
-       mi = n
-    elif (n < mn):
-       mn = n
-  
+    
     if (m == 1):
-      print(f'TAMANHO DE {n}: {f_tam(n)}')
+      print(f'TAMANHO DE {n}: {sf.f_tam(n)}')
     elif (m == 2):
-      print(f'INVERSO DE {n}: {f_inverte(n)}')
+      print(f'INVERSO DE {n}: {sf.f_inverte(n)}')
     elif (m == 3):
-      if (f_capicua(n)):
+      if (sf.f_capicua(n)):
         print(f'{n} É CAPICUA')
       else:
         print(f'{n} NÃO É CAPICUA')
     elif (m == 4):
-      if (f_verificaArmstrong(n)):
+      if (sf.f_verificaArmstrong(n)):
         print(f'{n} É UM NÚMERO ARMSTRONG')
       else:
         print(f'{n} NÃO É UM NÚMERO ARMSTRONG')
     elif (m == 5):
-      if (f_primo(n)):
+      if (sf.f_primo(n)):
         print(f'{n} É PRIMO')
       else:
         print(f'{n} NÃO É PRIMO')
     elif (m == 6):
-      if (f_perfeito(n)):
+      if (sf.f_perfeito(n)):
         print(f'{n} É UM NÚMERO PERFEITO')
       else:
         print(f'{n} NÃO É UM NÚMERO PERFEITO')
     elif (m == 7):
-      print(f'{n} EM BINÁRIO: {f_decToBin(n)}')
+      print(f'{n} EM BINÁRIO: {sf.f_decToBin(n)}')
     elif (m == 8):
-      print(f'{n} EM HEXADECIMAL: {f_decToHex(n)}')
+      print(f'{n} EM HEXADECIMAL: {sf.f_decToHex(n)}')
     elif (m == 9):
-      print(f'MDC ({n},{f_inverte(n)}): {f_mdc(n,f_inverte(n))}')
+      print(f'MDC ({n},{sf.f_inverte(n)}): {sf.f_mdc(n,sf.f_inverte(n))}')
     elif (m == 10):
-      print(f'QUANTIDADE DE PRIMOS ENTRE {n},{f_inverte(n)}: {f_contPrimo(n,f_inverte(n))}')
+      print(f'QUANTIDADE DE PRIMOS ENTRE {n},{sf.f_inverte(n)}: {sf.f_contPrimo(n,sf.f_inverte(n))}')
     m = int(input())
-    while (not f_verificaOpcao(m)):
+    while (not sf.f_verificaOpcao(m)):
       m = int(input(" OPCAO INVALIDA. TENTE OUTRA VEZ: "))
   return 0
 
